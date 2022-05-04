d532c764423e8ceda7ac42907d6495cb3124726b
Sir Mishaa <mishapro@mm.st>
2022-05-04 10:56:20 +0200

    :card_file_box: add userLead and address migration and change default env file

---
+++ database/migrations/2022_05_04_073820_create_address.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::create('addresses', function (Blueprint $table) {
+            $table->id();
+            $table->string('country');
+            $table->string('country_iso');
+            $table->string('street');
+            $table->string('postal_code');
+            $table->string('city');
+            $table->string('province');
+            $table->timestampsTz();
+
+            $table->foreignId('user_lead_id')
+                ->constrained('user_leads')
+                ->onDelete('cascade')
+                ->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('address');
+    }
+};
